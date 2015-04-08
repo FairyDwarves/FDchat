0bd67b0749b0e6f780b726cd6e1faea066db3842
AlexV <asmodehn@gmail.com>
2015-04-08 11:05:45 +0900

    fixed some problems with promises. now error is displayed when we cannot connect to backend

--- src/app/backend/FDrestSDK.js
+++ src/app/backend/FDrestSDK.js
@@ -4,57 +4,40 @@
 
 define([
     'dojo/_base/declare',
-    'dijit/ProgressBar',
+    'dojo/_base/lang',
+    'dojox/mobile/ProgressBar',
     './FDrestclient'
-], function(declare,ProgressBar,RestClient){
-	return declare(null, {
-	    log: function(msg){
+], function(declare,lang,ProgressBar,RestClient){
+    return declare(null, {
+        log: function(msg){
             console.log( msg );
-	    },
-	    getLoadingBar: function() {
-	        if (! this._loadingBar) {
-	            this._loadingBar = new ProgressBar({
-                    style: 'width: 300px'
-                });
-	        }
-	        return this._loadingBar;
-	    },
-        setLoadingBar: function(pct) {
-            this.getLoadingBar().set('value', pct % 100);
         },
-        initialize: function() {            
+        initialize: function() {
             if (! this._client ){
                 this._client=new RestClient();
             }
-            var self=this;
             return this._client.isAvailable()
-            .then(function(data){
-                // do something with handled data
-                self.log('/rest/monitoring/isAlive done -> ' + data);
-                //TODO : keep going...
-                        
-                //Load this app online configuration
-	            //TODO
-	
-	            //Discover the chat service
-	            //TODO
-	            
-	            this.resolve(data);//this promise is fulfilled.
-	            
-            }, function(err){
-                // handle an error condition
-                self.log('/rest/monitoring/isAlive error -> ' + err);
-                //TODO : cancel loading now.
-                
-                //propagate the error
-                this.reject(err);// this promise is rejected.
-                
-            }, function(update){
-                // handle a progress event
-                self.log('/rest/monitoring/isAlive update -> ' + update);
-                self.setLoadingBar(100);//set loading to 100%
-            });
+                .then(lang.hitch(this,function(data){
+                    // do something with handled data
+                    //TODO : keep going...
+
+                    //Load this app online configuration
+                    //TODO
+
+                    //Discover the chat service
+                    //TODO
+
+                    return data;//success
+
+                }), lang.hitch(this,function(err){
+                    // handle an error condition
+                    //TODO : cancel loading now.
+
+                    //propagate the error
+                    throw err;//error
+
+                }));// then hitch
         }
-	});
-});
+    });//declare
+});//define
 
--- src/app/main.js
+++ src/app/main.js
@@ -22,94 +22,98 @@
  * More information about everything described about the loader throughout this file can be found at
  * <http://dojotoolkit.org/reference-guide/loader/amd.html>.
  */
-define([ 'dojo/_base/window', './dialog/LoginDialog', './dialog/ErrorDialog', './backend/FDrestSDK', 'dojo/domReady!' ], function (window, LoginDialog, ErrorDialog, FDrestSDK) {
-	var app = {
-	    // Application Constructor
-	    initialize: function() {
-		this.bindEvents();
-	    },
-	    // Bind Event Listeners
-	    //
-	    // Bind any events that are required on startup. Common events are:
-	    // 'load', 'deviceready', 'offline', and 'online'.
-	    bindEvents: function() {
-		document.addEventListener('deviceready', this.onDeviceReady, false);
-	    },
-	    // deviceready Event Handler
-	    //
-	    // The scope of 'this' is the event. In order to call the 'receivedEvent'
-	    // function, we must explicitly call 'app.receivedEvent(...);'
-	    onDeviceReady: function() {
-		app.receivedEvent('deviceready');
-	    },
-	    // Update DOM on a Received Event
-	    receivedEvent: function(id) {
-		var parentElement = document.getElementById(id);
-		var listeningElement = parentElement.querySelector('.listening');
-		var receivedElement = parentElement.querySelector('.received');
+define([ 'dojo/_base/declare',
+    'dojo/_base/lang',
+    './ui',
+    './backend/FDrestSDK',
+    'dojo/domReady!',
+], function (declare, lang, UI, FDrestSDK) {
+    return {
+        _fdrestsdk: undefined,
+        //access the FDrest SDK
+        getFDrestSDK: function() {
+            if (! this._fdrestsdk){
+                this._fdrestsdk = new FDrestSDK();
+            }
+            return this._fdrestsdk;
+        },
+        _appUI: undefined,
+        //access the FDchat UI
+        getAppUI: function() {
+            if (! this._appUI){
+                this._appUI = new UI();
+            }
+            return this._appUI;
+        },
+        // Application Starter
+        start: function() {
+            this.bindEvents();
 
-		listeningElement.setAttribute('style', 'display:none;');
-		receivedElement.setAttribute('style', 'display:block;');
+            this.getAppUI().showLoading();
+            var initreq = this.getFDrestSDK().initialize();
 
-		console.log('Received Event: ' + id);
-	    }
-	};
+            // performing "callbacks" with the process:
+            initreq.then(lang.hitch(this, function(value){
+                // Do something when the process completes
 
-    //initializing app on device/browser
-	app.initialize();
-	
-	// Now that the app is loaded, we'll add an extra CSS class to the body to hide the loading message. Note that we
-	// could also have used `dojo/dom-class` to do this, but for very simple one-off operations like these there is
-	// usually no good reason to load an extra module.
-	document.body.className += ' loaded';
-	
-	//Check our backend service availability
-    app.FDSDK = new FDrestSDK();
-    app.FDSDK.getLoadingBar().placeAt(window.body()).startup();
-    
-	var deferred = app.FDSDK.initialize();
-	
-	    //TMP : display error message
-        app.dialog = new ErrorDialog().placeAt(document.body);
-        app.dialog.startup();
-        app.dialog.show();
-	
-	// performing "callbacks" with the process:
-    deferred.then(function(value){
-        // Do something when the process completes
+                this.getAppUI().hideLoading();
+                //Display the LoginDialog //TODO : login into chat or into REST ??
 
-        app.FDSDK.getLoadingBar().destroy();
-	    //Display the LoginDialog //TODO : login into chat or into REST ??
-	
-	    // Create a new instance of our custom Dijit dialog and place it in the DOM
-	    ////////app.dialog = new LoginDialog().placeAt(document.body);
+                // Create a new instance of our custom Dijit dialog and place it in the DOM
+                ////////app.dialog = new LoginDialog().placeAt(document.body);
 
-	    // It is important to remember to always call startup on widgets after you have added them to the DOM.
-	    // It will not hurt if you do it twice, but things will often not work right if you forget to do it
-	    ////////app.dialog.startup();
+                // It is important to remember to always call startup on widgets after you have added them to the DOM.
+                // It will not hurt if you do it twice, but things will often not work right if you forget to do it
+                ////////app.dialog.startup();
 
-	    // And now we just show the dialog to demonstrate that, yes, the example app has loaded successfully
-	    ////////app.dialog.show();
-            
-        
-        
-    }, function(err){
-        // Do something when the process errors out
-        
-        //TODO : display error message
-        app.dialog = new ErrorDialog().placeAt(document.body);
-        app.dialog.startup();
-        app.dialog.show();
-    });
-	
+                // And now we just show the dialog to demonstrate that, yes, the example app has loaded successfully
+                ////////app.dialog.show();
 
-	
-	
-	
-	
 
-	// Returning a value from an AMD module means that it becomes the value of the module. In this case, we return
-	// the `app` object, which means that other parts of the application that require `app/main` could get a reference
-	// to the dialog
-	return app;
-});
+
+            }), lang.hitch(this,function(err){
+                // Do something when the process errors out
+
+                //display error message
+                this.getAppUI().showError(err);
+
+            }));// then hitch
+
+
+
+            // Now that the app is loaded, we'll add an extra CSS class to the body to hide the loading message. Note that we
+            // could also have used `dojo/dom-class` to do this, but for very simple one-off operations like these there is
+            // usually no good reason to load an extra module.
+            document.body.className += ' loaded';
+        },
+
+
+        // Update DOM on a Received Event
+        receivedEvent: function(id) {
+            var parentElement = document.getElementById(id);
+            var listeningElement = parentElement.querySelector('.listening');
+            var receivedElement = parentElement.querySelector('.received');
+
+            listeningElement.setAttribute('style', 'display:none;');
+            receivedElement.setAttribute('style', 'display:block;');
+
+            console.log('Received Event: ' + id);
+        },
+        // deviceready Event Handler
+        //
+        // The scope of 'this' is the event. In order to call the 'receivedEvent'
+        // function, we must explicitly call 'app.receivedEvent(...);'
+        onDeviceReady: function() {
+            this.receivedEvent('deviceready');
+        },
+        // Bind Event Listeners
+        //
+        // Bind any events that are required on startup. Common events are:
+        // 'load', 'deviceready', 'offline', and 'online'.
+        bindEvents: function() {
+            document.addEventListener('deviceready', this.onDeviceReady, false);
+        },
+
+    };
+
+}); //define
